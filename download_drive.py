d1dbad8352c06e6cb46d616dfc7faff6d020c4af
DuyNguyen <duyngyn12.0404@gmail.com>
2025-11-16 14:35:52 +0700

    evaluate spamo

---
+++ download_drive.py
@@ -0,0 +1,20 @@
+import gdown
+import os
+
+# Thư mục lưu file
+save_dir = "/workspace/ai_intern/Kha/SpaMo/features/vit_feat_Phoenix14T"
+os.makedirs(save_dir, exist_ok=True)
+
+# File ID từ link Google Drive mới
+file_id = "1OuPHH80YTeeRPEgbeYb0_KwQiA8Xfk2L"
+
+# Đặt tên file tải về (đuôi .ckpt)
+output_path = os.path.join(save_dir, "spa_feat_p14t.zip")
+
+# URL tải trực tiếp
+url = f"https://drive.google.com/uc?id={file_id}"
+
+# Tải file
+gdown.download(url, output_path, quiet=False)
+
+print(f"✅ File đã được tải về: {output_path}")
